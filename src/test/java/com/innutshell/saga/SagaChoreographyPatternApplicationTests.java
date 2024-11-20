997321c28e8c7862bddfc2666251b57ee85121e6
srinivasrao-dumpala <srinivasarao.dumpala@gmail.com>
2024-11-19 22:01:04 -0500

    inital Setup

---
+++ src/test/java/com/innutshell/saga/SagaChoreographyPatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.innutshell.saga;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SagaChoreographyPatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
