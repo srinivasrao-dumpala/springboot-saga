11b365895290ef983407f5fd9ca860996dccc926
srinivasrao-dumpala <srinivasarao.dumpala@gmail.com>
2024-11-19 22:15:01 -0500

    Common DTO Services

--- common-dtos/src/main/java/com/innutshell/CommonsDtoApplication.java
+++ common-dtos/src/main/java/com/innutshell/CommonsDtoApplication.java
@@ -1,6 +1,6 @@
 package com.innutshell;
 
-public class Main {
+public class CommonsDtoApplication {
     public static void main(String[] args) {
         System.out.println("Hello world!");
     }
